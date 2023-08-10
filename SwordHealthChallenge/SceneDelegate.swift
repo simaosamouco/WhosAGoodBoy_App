5a336a9213e141e6d204035e364dca3e5a38b98c
Simão Neves Samouco <simao.samouco@hotmail.com>
2023-08-10 10:46:30 +0100

    TabBar set up with two view controllers

--- SwordHealthChallenge/SceneDelegate.swift
+++ SwordHealthChallenge/SceneDelegate.swift
@@ -15,8 +15,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: UIScreen.main.bounds)
-        let vc = ViewController()
-        self.window?.rootViewController = vc
+        let tabBar = TabBarController()
+        self.window?.rootViewController = tabBar
         window?.makeKeyAndVisible()
         window?.windowScene = windowScene
     }
---
+++ SwordHealthChallenge/View Controllers/TabBarController.swift
@@ -0,0 +1,53 @@
+//
+//  TabBarController.swift
+//  SwordHealthChallenge
+//
+//  Created by Simão Neves Samouco on 10/08/2023.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().barTintColor = .darkGray
+        UITabBar.appearance().tintColor = .black
+        setUpVCs()
+        
+    }
+    
+    func setUpVCs() {
+        viewControllers = [
+            createNavController(for: ImagesViewController(),
+                                title: NSLocalizedString("Images", comment: ""),
+                                image: UIImage(systemName: "photo.stack")!),
+            createNavController(for: NamesViewController(),
+                                title: NSLocalizedString("Names", comment: ""),
+                                image: UIImage(systemName: "a.magnify")!)
+        ]
+    }
+    
+    func createNavController(for rootViewController: UIViewController,
+                                         title: String,
+                                         image: UIImage) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        navController.navigationBar.prefersLargeTitles = false
+        rootViewController.navigationItem.title = title
+        return navController
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

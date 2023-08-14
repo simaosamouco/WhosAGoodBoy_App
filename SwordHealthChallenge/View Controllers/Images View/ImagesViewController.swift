d047a2953973c6fde2257de80ade34b597f4b8b2
Simão Neves Samouco <simao.samouco@hotmail.com>
2023-08-14 09:09:15 +0100

    ImageViewController update

--- SwordHealthChallenge/View Controllers/Images View/ImagesViewController.swift
+++ SwordHealthChallenge/View Controllers/Images View/ImagesViewController.swift
@@ -12,13 +12,34 @@ import RxCocoa
 
 class ImagesViewController: UIViewController, UICollectionViewDelegate {
     
-    private lazy var spinner: SpinningLoaderView = {
+    // MARK: - Properties
+    
+    private var spinnerViewHeight: Constraint?
+    private var isChangingLayout = false
+    
+    private var listLayout = ListFlowLayout()
+    private var gridLayout = GridFlowLayout()
+    private var isGridLayout: Bool = true {
+        didSet {
+            self.switchLayoutButton.setImage(self.isGridLayout ?
+                                 UIImage(systemName: "line.3.horizontal") :
+                                    UIImage(systemName: "square.grid.3x3"),
+                                 for: .normal)
+        }
+    }
+    
+    private let bag = DisposeBag()
+    var viewModel: ImagesViewModel!
+    
+    // MARK: - UI Elements
+    
+    private lazy var loaderView: SpinningLoaderView = {
         let view = SpinningLoaderView()
         view.startAnimating()
         return view
     }()
     
-    private lazy var spinnerView: UIView = {
+    private lazy var collectionViewFooterLoader: UIView = {
         let ui = UIView()
         let spinner = UIActivityIndicatorView()
         spinner.center = ui.center
@@ -31,18 +52,16 @@ class ImagesViewController: UIViewController, UICollectionViewDelegate {
         return ui
     }()
     
-    private var spinnerViewHeight: Constraint?
-    
-    private lazy var collectionVieww: UICollectionView = {
+    private lazy var collectionView: UICollectionView = {
         let layout = gridLayout
         layout.scrollDirection = .vertical
-        var cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
         cv.backgroundColor = .white
         cv.showsVerticalScrollIndicator = false
         return cv
     }()
     
-    private lazy var button: UIButton = {
+    private lazy var switchLayoutButton: UIButton = {
         let bt = UIButton()
         bt.setImage(UIImage(systemName: "line.3.horizontal"), for: .normal)
         bt.tintColor = .white
@@ -59,7 +78,7 @@ class ImagesViewController: UIViewController, UICollectionViewDelegate {
         return bt
     }()
     
-    private lazy var orderButton: UIButton = {
+    private lazy var orderAlphabeticallyButton: UIButton = {
         let bt = UIButton()
         bt.setImage(UIImage(systemName: "textformat.abc"), for: .normal)
         bt.tintColor = .white
@@ -77,23 +96,7 @@ class ImagesViewController: UIViewController, UICollectionViewDelegate {
         return bt
     }()
     
-    var listLayout = ListFlowLayout()
-    var gridLayout = GridFlowLayout()
-    
-    var isGridLayout: Bool = true {
-        didSet {
-            self.button.setImage(self.isGridLayout ?
-                                 UIImage(systemName: "line.3.horizontal") :
-                                    UIImage(systemName: "square.grid.3x3"),
-                                 for: .normal)
-        }
-    }
-    //This variable is need because when the collection view flow layout changes it triggers pagination
-    var isChangingLayout = false
-
-    var viewModel: ImagesViewModel!
-    
-    private let bag = DisposeBag()
+    // MARK: - Initialization
     
     init(viewModel: ImagesViewModel) {
         self.viewModel = viewModel
@@ -104,26 +107,66 @@ class ImagesViewController: UIViewController, UICollectionViewDelegate {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .systemBackground
-        self.viewModel.getDogsList()
-        collectionVieww.register(DogImageCollectionViewCell.self, forCellWithReuseIdentifier: "dogCell")
-        collectionVieww.delegate = self
-        setUpViews()
-        setUpBindings()
-        spinnerView.isHidden = true
+        setupUI()
+        setupBindings()
+        viewModel.getDogsList()
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        collectionVieww.reloadData()
+        super.viewWillAppear(animated)
+        collectionView.reloadData()
     }
     
-    func setUpBindings() {
+    // MARK: - UI Setup
+    
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(collectionViewFooterLoader)
+        view.addSubview(collectionView)
+        view.addSubview(switchLayoutButton)
+        view.addSubview(orderAlphabeticallyButton)
+        view.addSubview(loaderView)
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.leading.equalTo(view.safeAreaLayoutGuide).offset(16)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).offset(-16)
+            make.bottom.equalTo(collectionViewFooterLoader.snp.top)
+        }
+        switchLayoutButton.snp.makeConstraints { make in
+            make.width.height.equalTo(50)
+            make.bottom.equalTo(orderAlphabeticallyButton.snp.top).offset(-16)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).offset(-16)
+        }
+        orderAlphabeticallyButton.snp.makeConstraints { make in
+            make.width.height.equalTo(50)
+            make.bottom.trailing.equalTo(view.safeAreaLayoutGuide).offset(-16)
+        }
+        collectionViewFooterLoader.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.top.equalTo(collectionView.snp.bottom)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+            spinnerViewHeight = make.height.equalTo(0).constraint
+        }
+        
+        loaderView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Data Bindings
+    
+    private func setupBindings() {
+        
+        collectionView.register(DogImageCollectionViewCell.self, forCellWithReuseIdentifier: "dogCell")
+        collectionView.delegate = self
+        
         viewModel.dogsProfileList
-            .bind(to: collectionVieww.rx.items(cellIdentifier: "dogCell", cellType: DogImageCollectionViewCell.self)) { [weak self] index, dog, cell in
-                //guard let self = self else { return } // Capture self strongly
-                
+            .bind(to: collectionView.rx.items(cellIdentifier: "dogCell", cellType: DogImageCollectionViewCell.self)) { index, dog, cell in
                 cell.backgroundColor = .systemBackground
                 cell.layer.cornerRadius = 15
                 cell.layer.shadowColor = UIColor.black.cgColor
@@ -138,81 +181,52 @@ class ImagesViewController: UIViewController, UICollectionViewDelegate {
             }
             .disposed(by: bag)
         
-        collectionVieww.rx.modelSelected(DogProfile.self).subscribe(onNext: { dogProfile in
+        collectionView.rx.modelSelected(DogProfile.self).subscribe(onNext: { dogProfile in
             print(dogProfile.breedName)
             self.viewModel.cellSelected(dogProfile)
         }).disposed(by: bag)
         
         viewModel.actionNavigateToDetailView
-                    .subscribe(onNext: { [weak self] vc in
-                        self?.navigationController?.pushViewController(vc, animated: true)
-                    })
-                    .disposed(by: bag)
+            .subscribe(onNext: { [weak self] vc in
+                self?.navigationController?.pushViewController(vc, animated: true)
+            })
+            .disposed(by: bag)
         
         viewModel.isFetching
-                    .subscribe(onNext: { [weak self] isFetching in
-                        if isFetching {
-                            self?.spinnerView.isHidden = false
-                            self?.spinnerViewHeight?.update(offset: 50)
-                        } else {
-                            self?.spinner.isHidden = true
-                            self?.spinnerView.isHidden = true
-                            self?.spinnerViewHeight?.update(offset: 0)
-                        }
-                    })
-                    .disposed(by: bag)
+            .subscribe(onNext: { [weak self] isFetching in
+                if isFetching {
+                    self?.collectionViewFooterLoader.isHidden = false
+                    self?.spinnerViewHeight?.update(offset: 50)
+                } else {
+                    self?.loaderView.isHidden = true
+                    self?.collectionViewFooterLoader.isHidden = true
+                    self?.spinnerViewHeight?.update(offset: 0)
+                }
+            })
+            .disposed(by: bag)
     }
     
-    func setUpViews() {
-        view.addSubview(spinnerView)
-        view.addSubview(collectionVieww)
-        view.addSubview(button)
-        view.addSubview(orderButton)
-        view.addSubview(spinner)
-        
-        collectionVieww.snp.makeConstraints { make in
-            make.top.leading.equalTo(view.safeAreaLayoutGuide).offset(16)
-            make.trailing.equalTo(view.safeAreaLayoutGuide).offset(-16)
-            make.bottom.equalTo(spinnerView.snp.top)
-        }
-        button.snp.makeConstraints { make in
-            make.width.height.equalTo(50)
-            make.bottom.equalTo(orderButton.snp.top).offset(-16)
-            make.trailing.equalTo(view.safeAreaLayoutGuide).offset(-16)
-        }
-        orderButton.snp.makeConstraints { make in
-            make.width.height.equalTo(50)
-            make.bottom.trailing.equalTo(view.safeAreaLayoutGuide).offset(-16)
-        }
-        spinnerView.snp.makeConstraints { make in
-            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
-            make.top.equalTo(collectionVieww.snp.bottom)
-            make.bottom.equalTo(view.safeAreaLayoutGuide)
-            spinnerViewHeight = make.height.equalTo(0).constraint
-        }
-        
-        spinner.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
-    }
+    // MARK: - Button Actions
     
-    @objc func switchLayoutPressed(_ sender: UIButton) {
+    @objc private func switchLayoutPressed(_ sender: UIButton) {
         isGridLayout.toggle()
         let layout = isGridLayout ? gridLayout : listLayout
         isChangingLayout = true
-        collectionVieww.collectionViewLayout.invalidateLayout()
-        collectionVieww.setCollectionViewLayout(layout, animated: true)
+        collectionView.collectionViewLayout.invalidateLayout()
+        collectionView.setCollectionViewLayout(layout, animated: true)
         self.isChangingLayout = false
     }
     
-    @objc func orderPressed(_ sender: UIButton) {
+    @objc private func orderPressed(_ sender: UIButton) {
         viewModel.orderListAlphabetically()
     }
     
+    // MARK: - Scroll View Delegate
+    
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         if isChangingLayout {
-                return
-            }
+            return
+        }
         let offsetY = scrollView.contentOffset.y
         let contentHeight = scrollView.contentSize.height
         let distanceFromBottom = contentHeight - offsetY

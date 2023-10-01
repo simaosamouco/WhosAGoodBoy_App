591c8c396d47ab018677695bc8ca252baa15b31c
Simão Neves Samouco <simao.samouco@hotmail.com>
2023-10-01 12:57:27 +0100

    Updated to use Swift Package Manager

--- Pods/Target Support Files/Pods-SwordHealthChallengeTests/Pods-SwordHealthChallengeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwordHealthChallengeTests : NSObject
-@end
-@implementation PodsDummy_Pods_SwordHealthChallengeTests
-@end
--- SwordHealthChallenge/Managers/ServicesManager.swift
+++ SwordHealthChallenge/Managers/ServicesManager.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 import Alamofire
 
 typealias APICompletion<T: Decodable> = (Result<T, Error>) -> Void
